9256f26755831fce9d51915d35930c6deb125544
JL6H <45859940+JL6H@users.noreply.github.com>
2019-02-21 10:46:24 +0000

    Add files via upload

---
+++ simple-music-master/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NTQ4MDkxODI0NjE5OTEzMjI2.D1ASSA.LknOKD6wIkGdCff0nv-We9_wuEk', 
+    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
+    prefix: '1',
+    devs: ['400692843582062604']
+}
